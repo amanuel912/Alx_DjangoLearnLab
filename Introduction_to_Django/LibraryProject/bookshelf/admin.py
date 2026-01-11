5d46632d2523c4e249747b69c800bbeedac305cf
amanuel912 <amanu.hk82@gmail.com>
2026-01-11 23:31:11 +0900

    Refactor admin.py to define BookAdmin class and register Book model with ModelAdmin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,3 +5,8 @@ import bookshelf
 # Register your models here.
 admin.site.register(bookshelf.models.Book)
 admin.ModelAdmin
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("author", "publication_year")
+    search_fields = ("title", "author")
