149384876d175832faf2a1f4d9c76bc8e7a4491b
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 13:44:23 +0900

    Fix books_in_library function to correctly return all books without arguments

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ def books_by_author(author_name):
     return Book.objects.filter(author = author)
 def books_in_library(library_name):
     library = Library.objects.get(name = library_name)
-    return library.books.all(library)
+    return library.books.all()
 def librarian_for_library(library_name):
     library = Library.objects.get(name = library_name)
     return Librarian.objects.get(library = library)
