1c174e2d8c8f9fc2f57140431b5e4fb9d29bd151
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 15:57:02 +0900

    Fix import statement for DetailView in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from relationship_app.models import Book,Author,Library,Librarian   
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from .models import Library
 # Create your views here.
 def list_books(request):
