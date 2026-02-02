743fba563fccfea24420eaba0e53d571b0f7dd4d
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 13:41:56 +0900

    Fix typo in books_in_library function to correctly retrieve library by name

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ def books_by_author(author_name):
     author = Author.objects.get(name = author_name)
     return Book.objects.filter(author = author)
 def books_in_library(library_name):
-    library = Library.objects.getname = library_name
+    library = Library.objects.get(name = library_name)
     return library.books.all()
 def librarian_for_library(library_name):
     library = Library.objects.get(name = library_name)
