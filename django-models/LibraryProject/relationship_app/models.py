cca4a248de7d4130c9d0cef2346c1452fe02bdda
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 13:28:01 +0900

    Add __str__ methods to models in relationship_app for better string representation

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -3,14 +3,23 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class BookModel(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
+
 class LibraryModel(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(BookModel)
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(LibraryModel, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
