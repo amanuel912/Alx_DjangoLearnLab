62661d45a5c6a8f5142fff054c93404b9362b59d
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 13:29:42 +0900

    Rename BookModel to Book and LibraryModel to Library in models.py; update related references in migrations and relationships

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,20 +6,20 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class BookModel(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     def __str__(self):
         return self.title
 
-class LibraryModel(models.Model):
+class Library(models.Model):
     name = models.CharField(max_length=100)
-    books = models.ManyToManyField(BookModel)
+    books = models.ManyToManyField(Book)
     def __str__(self):
         return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
-    library = models.OneToOneField(LibraryModel, on_delete=models.CASCADE)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
