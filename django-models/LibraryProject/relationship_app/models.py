110a56de1609c587acc420462285236b0235b8d5
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 13:25:34 +0900

    Add models for Author, BookModel, LibraryModel, and Librarian in relationship_app

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class BookModel(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+class LibraryModel(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(BookModel)
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(LibraryModel, on_delete=models.CASCADE)
