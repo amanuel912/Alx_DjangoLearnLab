0e2ca051d76fc356652769f18966be91ec2b473b
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 18:15:28 +0900

    Update URL patterns to use class-based views for login and logout

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import login_view, logout_view, register_view
+from django.contrib.auth import views
+from . import views as app_views
+
 
 urlpatterns = [ path('books/', list_books, name = 'list_books'),
                path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-               path('login/', login_view, name='login'),
-               path('logout/', logout_view, name='logout'),
-                path('register/', register_view, name='register'),]
+               path("register/", app_views.register, name="register"),
+
+    path(
+        "login/",
+        views.LoginView.as_view(template_name="relationship_app/login.html"),
+        name="login",
+    ),
+
+    path(
+        "logout/",
+        views.LogoutView.as_view(template_name="relationship_app/logout.html"),
+        name="logout",
+    ),]
