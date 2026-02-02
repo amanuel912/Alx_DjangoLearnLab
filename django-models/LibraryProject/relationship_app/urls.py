2892f8b0327341b389ac942bd195f47b881aa7b6
amanuel912 <amanu.hk82@gmail.com>
2026-02-02 19:06:03 +0900

    Refactor URL patterns to streamline imports and enhance readability

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,25 +1,30 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
-from .views import login_view, logout_view, register
-from django.contrib.auth import views
-from . import views as app_views
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
-urlpatterns = [ path('books/', list_books, name = 'list_books'),
-               path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-               path("register/", app_views.register, name="register"),
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+
+    path('register/', views.register, name='register'),
 
     path(
-        "login/",
-        views.LoginView.as_view(template_name="relationship_app/login.html"),
-        name="login",
+        'login/',
+        auth_views.LoginView.as_view(
+            template_name='relationship_app/login.html'
+        ),
+        name='login',
     ),
 
     path(
-        "logout/",
-        views.LogoutView.as_view(template_name="relationship_app/logout.html"),
-        name="logout",
+        'logout/',
+        auth_views.LogoutView.as_view(
+            template_name='relationship_app/logout.html'
+        ),
+        name='logout',
     ),
-        path('admin/', app_views.admin_view, name='admin_view'),
-    path('librarian/', app_views.librarian_view, name='librarian_view'),
-    path('member/', app_views.member_view, name='member_view'),]
+
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
